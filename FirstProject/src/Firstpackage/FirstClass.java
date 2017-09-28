e865f81f5127a578bb0177b5256ebbb4f53c5114
Bharat <bharatchow.chaparala@gmail.com>
2017-09-28 15:19:19 -0500

    commited by bharat sep28

---
+++ FirstProject/src/Firstpackage/FirstClass.java
@@ -0,0 +1,21 @@
+package Firstpackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class FirstClass {
+	
+	@Test
+	public void testJenkins(){
+		System.out.println("welcome to jenkins world123");
+		
+		System.setProperty("webdriver.gecko.driver","C:\\BrowserDrivers\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.facebook.com");
+		System.out.println(driver.getTitle());
+		driver.quit();
+
+	}
+
+}
